c388470d231e5da21c765a034e8bd26eeac25ad3
yushuai1 <31026026+yushuai1@users.noreply.github.com>
2023-06-01 18:07:28 +0800

    Update net_util.go

--- nets/net_util.go
+++ nets/net_util.go
@@ -1,23 +1,25 @@
 package nets
 
 import (
-	"fmt"
 	"net"
 	"os"
 	"strings"
+	"sync"
 )
 
-func GetIP() string {
-	conn, err := net.Dial("udp", "8.8.8.8:53")
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-	// 192.168.1.20:61085
-	ip := strings.Split(localAddr.String(), ":")[0]
+var myip string
+var once sync.Once
 
-	return ip
+func GetIP() string {
+
+	once.Do(func() {
+		conn, _ := net.Dial("udp", "8.8.8.8:80")
+		defer conn.Close()
+		localAddr := conn.LocalAddr().String()
+		idx := strings.LastIndex(localAddr, ":")
+		myip = localAddr[0:idx]
+	})
+	return myip
 }
 func GetMacAddr() string {
 	interfaces, err := net.Interfaces()
